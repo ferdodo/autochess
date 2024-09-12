da93aa96cfd6dba89fc9de3bfc8a82d9fb68a5ff
Thomas Riffard <ferdodo@hotmail.fr>
2024-09-12 08:29:38 +0200

    💦 Remove extra camera creation

--- core/utils/create-renderer.ts
+++ core/utils/create-renderer.ts
@@ -1,12 +1,10 @@
 import { WebGLRenderer } from "three";
 import type { Block } from "blockwise";
-import { createCamera } from "./create-camera";
 import type { Context } from "../types/context";
 import { notifyFrameStart, notifyFrameEnd } from "./observe-fps";
 
 export function createRenderer(windowDimentions: Block, context: Context) {
 	const renderer = new WebGLRenderer();
-	context.camera = createCamera();
 	renderer.setSize(windowDimentions.w, windowDimentions.h);
 	renderer.setClearColor(0x0000ff);
 	document.body.appendChild(renderer.domElement);
