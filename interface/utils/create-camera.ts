4ae4a8905e6ecb31a0400517ba756a222b6b44f2
Thomas Riffard <ferdodo@hotmail.fr>
2024-09-28 11:12:53 +0200

    📷 Compensate camera fov based on viewport ratio

--- interface/utils/create-camera.ts
+++ interface/utils/create-camera.ts
@@ -1,33 +1,10 @@
-import { PerspectiveCamera, OrthographicCamera } from "three";
+import { PerspectiveCamera } from "three";
 
-export function createCamera(): PerspectiveCamera | OrthographicCamera {
+export function createCamera(): PerspectiveCamera {
 	const windowRatio = window.innerWidth / window.innerHeight;
-	if (Math.random() > 2) {
-		const camera = new PerspectiveCamera(10, windowRatio, 0.1, 1000);
-		camera.position.z = 6;
-		camera.position.y = 6;
-		camera.rotation.x = -0.75;
-		return camera;
-	}
-
-	if (Math.random() > 2) {
-		const frustumSize = 1;
-
-		const camera = new OrthographicCamera(
-			(frustumSize * windowRatio) / -2,
-			(frustumSize * windowRatio) / 2,
-			frustumSize / 2,
-			frustumSize / -2,
-			0.1,
-			1000,
-		);
-		camera.position.z = 3;
-		camera.position.y = 3;
-		camera.rotation.x = -0.7;
-		return camera;
-	}
-
-	const camera = new PerspectiveCamera(25, windowRatio, 0.1, 1000);
+	const compensationValue = Math.max(0, (-windowRatio + 1.6) / 0.035);
+	const fov = 25 + compensationValue;
+	const camera = new PerspectiveCamera(fov, windowRatio, 0.1, 1000);
 	camera.position.z = 2;
 	camera.position.y = 2;
 	camera.rotation.x = -0.7;
