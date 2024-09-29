bb7dce187d96b25b71a89809b24a509678687170
Thomas Riffard <ferdodo@hotmail.fr>
2024-09-29 14:46:57 +0200

    📈 Tweak apect ratio compensation

--- interface/utils/create-camera.ts
+++ interface/utils/create-camera.ts
@@ -2,7 +2,7 @@ import { PerspectiveCamera } from "three";
 
 export function createCamera(): PerspectiveCamera {
 	const windowRatio = window.innerWidth / window.innerHeight;
-	const compensationValue = Math.max(0, (-windowRatio + 1.6) / 0.035);
+	const compensationValue = Math.max(0, (-windowRatio + 1.6) / 0.055) ** 1.3;
 	const fov = 25 + compensationValue;
 	const camera = new PerspectiveCamera(fov, windowRatio, 0.1, 1000);
 	camera.position.z = 2;
