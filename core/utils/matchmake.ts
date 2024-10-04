9bb4509d89c809617b65ec9ef103906f33f93bfd
Thomas Riffard <ferdodo@hotmail.fr>
2024-10-04 13:12:03 +0200

    🤏Start game with less players

--- core/utils/matchmake.ts
+++ core/utils/matchmake.ts
@@ -2,17 +2,18 @@ import type { Subscription } from "rxjs";
 import { throttleTime } from "rxjs/operators";
 import { createGameId } from "./create-game-id";
 import type { BackContext } from "../types/back-context";
+import { merge, tap } from "rxjs";
 
 const MATCHMAKING_THROTTLE_TIME = 5000;
+const MATCHMAKING_LATE = 30000;
 
 export function matchmake({
 	queuerDataMapper,
 	gameDataMapper,
 }: BackContext): Subscription {
-	return queuerDataMapper
-		.observe()
-		.pipe(throttleTime(MATCHMAKING_THROTTLE_TIME))
-		.subscribe(async (queuers) => {
+	const startWhenEight$ = queuerDataMapper.observe().pipe(
+		throttleTime(MATCHMAKING_THROTTLE_TIME),
+		tap(async (queuers) => {
 			if (queuers.length > 8) {
 				const oldestFirst = [...queuers].sort(
 					(a, b) => a.createdAt - b.createdAt,
@@ -34,5 +35,35 @@ export function matchmake({
 					nicknames,
 				});
 			}
-		});
+		}),
+	);
+
+	const startWhenLate$ = queuerDataMapper.observe().pipe(
+		throttleTime(MATCHMAKING_LATE),
+		tap(async (queuers) => {
+			if (queuers.length > 1) {
+				const oldestFirst = [...queuers].sort(
+					(a, b) => a.createdAt - b.createdAt,
+				);
+
+				const players = oldestFirst.slice(0, 8);
+
+				const nicknames = players.reduce(
+					(acc, player) => {
+						acc[player.publicKey] = player.nickname;
+						return acc;
+					},
+					<Record<string, string>>{},
+				);
+
+				await gameDataMapper.createAndRemoveQueuers({
+					id: createGameId(players),
+					publicKeys: players.map((player) => player.publicKey),
+					nicknames,
+				});
+			}
+		}),
+	);
+
+	return merge(startWhenEight$, startWhenLate$).subscribe();
 }
