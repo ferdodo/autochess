f26f468be37187174a2951ccd5ca650b1d32aeff
Thomas Riffard <ferdodo@hotmail.fr>
2024-10-20 19:09:07 +0200

    🔣 First draft for game schema

--- core/types/game.ts
+++ core/types/game.ts
@@ -1,9 +1,11 @@
-import type { PublicKey } from "./public-key";
+import { publicKeySchema, type PublicKey } from "./public-key";
 import type { Hero } from "./hero";
 import type { Appellation } from "./appellation";
 import type { Playsig } from "./playsig";
 import type { Phase } from "./phase";
 import type { Level } from "./level";
+import { playsigSchema } from "./playsig";
+import { nicknameSchema } from "./nickname";
 
 export interface Game {
 	playsig: Playsig;
@@ -16,3 +18,33 @@ export interface Game {
 	playerLevel: Record<PublicKey, Level>;
 	phase: Phase;
 }
+
+export const gameSchema = {
+	type: "object",
+	required: [
+		"playsig",
+		"publicKeys",
+		"nicknames",
+		"playerHeroes",
+		"playerBenches",
+		"playerShops",
+		"playerMoney",
+		"playerLevel",
+		"phase",
+	],
+	properties: {
+		playsig: playsigSchema,
+		publicKeys: {
+			type: "array",
+			items: publicKeySchema,
+			minItems: 2,
+			maxItems: 8,
+		},
+		nicknames: {
+			type: "object",
+			additionalProperties: nicknameSchema,
+			minProperties: 2,
+			maxProperties: 8,
+		},
+	},
+};
