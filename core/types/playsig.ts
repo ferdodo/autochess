1f14297ab95f56aa624a8ce5a51f1ac2dfabebb4
Thomas Riffard <ferdodo@hotmail.fr>
2024-10-21 15:01:26 +0200

    🎮 Add schema checking for playsig

--- core/types/playsig.ts
+++ core/types/playsig.ts
@@ -1,3 +1,5 @@
+import type { FromSchema } from "json-schema-to-ts";
+
 export type Playsig = string;
 
 export const playsigSchema = {
@@ -6,3 +8,6 @@ export const playsigSchema = {
 	maxLength: 132,
 	pattern: "^[0-9a-fA-F]+$",
 } as const;
+
+const a: Playsig = {} as FromSchema<typeof playsigSchema>;
+const b: FromSchema<typeof playsigSchema> = {} as Playsig;
