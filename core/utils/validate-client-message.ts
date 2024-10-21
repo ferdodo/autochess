8916c73265c62121da716189bceb704942068359
Thomas Riffard <ferdodo@hotmail.fr>
2024-10-21 15:16:42 +0200

    🧹 Remove previous schema enforcement

--- core/utils/validate-client-message.ts
+++ core/utils/validate-client-message.ts
@@ -6,22 +6,6 @@ import { Draft07 } from "json-schema-library";
 export function validateClientMessage(
 	message: ClientMessage,
 ): [boolean, string] {
-	const errors = new Draft07(clientMessageSchema).validate(
-		enforceConsistency(message),
-	);
-
+	const errors = new Draft07(clientMessageSchema).validate(message);
 	return [!errors.length, JSON.stringify(errors, null, 2)];
-}
-
-/**
- * Noop function.
- *
- * Assignations between Message and FromSchema are enforcing
- * consistency between JSON-schema and Typescript interfaces
- * at compile time. ❤️
- */
-export function enforceConsistency(message: ClientMessage): ClientMessage {
-	const messageCastedFromSchema: FromSchema<typeof clientMessageSchema> =
-		message;
-	return messageCastedFromSchema;
 }
