a87be73a3a9332cf66e992f7628d127ca1b217dd
Thomas Riffard <ferdodo@hotmail.fr>
2024-12-16 09:34:38 +0100

    🦺 Trycatch signatures verification

--- back/utils/verify.ts
+++ back/utils/verify.ts
@@ -2,43 +2,47 @@ import type { Signed } from "core/types/signed";
 import { subtle } from "node:crypto";
 
 export async function verify<T>(payload: T & Signed): Promise<boolean> {
-	if (new Date() > new Date(payload.expiresAt)) {
+	try {
+		if (new Date() > new Date(payload.expiresAt)) {
+			return false;
+		}
+
+		const publicKey: string = payload.publicKey;
+		const signature: string = payload.signature;
+		const publicKeyBuffer: Buffer = Buffer.from(publicKey, "hex");
+
+		const ecKeyImportParams: EcKeyImportParams = {
+			name: "ECDSA",
+			namedCurve: "P-384",
+		};
+
+		const publicCryptoKey: CryptoKey = await subtle.importKey(
+			"spki",
+			publicKeyBuffer,
+			ecKeyImportParams,
+			false,
+			["verify"],
+		);
+
+		const ecdsaParams: EcdsaParams = { name: "ECDSA", hash: "SHA-384" };
+		const signatureBuffer: Buffer = Buffer.from(signature, "hex");
+
+		const payloadSerialised: string = JSON.stringify(
+			payload,
+			Object.keys(payload).sort(),
+		);
+
+		const payloadBuffer: Buffer = Buffer.from(payloadSerialised, "utf8");
+
+		const valid = subtle.verify(
+			ecdsaParams,
+			publicCryptoKey,
+			signatureBuffer,
+			payloadBuffer,
+		);
+
+		return valid;
+	} catch (_e) {
 		return false;
 	}
-
-	const publicKey: string = payload.publicKey;
-	const signature: string = payload.signature;
-	const publicKeyBuffer: Buffer = Buffer.from(publicKey, "hex");
-
-	const ecKeyImportParams: EcKeyImportParams = {
-		name: "ECDSA",
-		namedCurve: "P-384",
-	};
-
-	const publicCryptoKey: CryptoKey = await subtle.importKey(
-		"spki",
-		publicKeyBuffer,
-		ecKeyImportParams,
-		false,
-		["verify"],
-	);
-
-	const ecdsaParams: EcdsaParams = { name: "ECDSA", hash: "SHA-384" };
-	const signatureBuffer: Buffer = Buffer.from(signature, "hex");
-
-	const payloadSerialised: string = JSON.stringify(
-		payload,
-		Object.keys(payload).sort(),
-	);
-
-	const payloadBuffer: Buffer = Buffer.from(payloadSerialised, "utf8");
-
-	const result = subtle.verify(
-		ecdsaParams,
-		publicCryptoKey,
-		signatureBuffer,
-		payloadBuffer,
-	);
-
-	return result;
 }
