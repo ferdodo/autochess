fdff20b1e88a18e23f6ea4f823a623fea43ff342
Thomas Riffard <ferdodo@hotmail.fr>
2024-12-17 15:32:23 +0100

    🧪 Refactor and test matchmaking

---
+++ core/workflows/matchmake.test.ts
@@ -0,0 +1,87 @@
+import { test, expect } from "vitest";
+import { withTwoPlayerGameStarted } from "../fixtures/with-two-player-game-started.js";
+import { observeGame } from "../api/observe-game.js";
+import { firstValueFrom } from "rxjs";
+import { asNewPlayerConnect } from "../automations/as-new-player-connect.js";
+import { asPlayerInitiateGame } from "../automations/as-player-initiate-game.js";
+import { withServerStarted } from "../fixtures/with-server-started.js";
+
+test("Should create game with all players nicknames", async () => {
+	const testContext = await withTwoPlayerGameStarted();
+	const frontContext = testContext.frontContexts[0];
+
+	if (!frontContext) {
+		throw new Error("Front context not found !");
+	}
+
+	const game = await firstValueFrom(observeGame(frontContext));
+
+	const allNicknames = Object.values(testContext.frontContexts).map(
+		(frontContext) => frontContext.nickname,
+	);
+
+	expect(Object.values(game.nicknames)).toEqual(allNicknames);
+	expect(Object.values(game.nicknames).length).toBeTruthy();
+});
+
+test("Should create a game with 8 maximum players", async () => {
+	const testContext = withServerStarted();
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+	asNewPlayerConnect(testContext);
+
+	await Promise.any([
+		asPlayerInitiateGame(testContext, 0),
+		asPlayerInitiateGame(testContext, 1),
+		asPlayerInitiateGame(testContext, 2),
+		asPlayerInitiateGame(testContext, 3),
+		asPlayerInitiateGame(testContext, 4),
+		asPlayerInitiateGame(testContext, 5),
+		asPlayerInitiateGame(testContext, 6),
+		asPlayerInitiateGame(testContext, 7),
+		asPlayerInitiateGame(testContext, 8),
+		new Promise(() =>
+			setTimeout(() => {
+				testContext.skipMatchmakeLateDebounce();
+			}, 1),
+		),
+	]);
+
+	const frontContext = Object.values(testContext.frontContexts).filter(
+		(f) => f.playsig,
+	)[0];
+
+	if (!frontContext) {
+		throw new Error("Front context not found !");
+	}
+
+	const game = await firstValueFrom(observeGame(frontContext));
+
+	expect(Object.values(game.nicknames).length).toBe(8);
+});
+
+test("Should not start a game with 1 player", async () => {
+	const testContext = withServerStarted();
+	asNewPlayerConnect(testContext);
+
+	await expect(
+		() =>
+			new Promise((resolve, reject) => {
+				setTimeout(() => {
+					reject(new Error("Good error"));
+				}, 5);
+
+				asPlayerInitiateGame(testContext, 0).then(() => resolve(undefined));
+
+				setTimeout(() => {
+					testContext.skipMatchmakeLateDebounce();
+				}, 1);
+			}),
+	).rejects.toThrow("Good error");
+}, 1000);
--- core/workflows/matchmake.ts
+++ core/workflows/matchmake.ts
@@ -1,8 +1,7 @@
 import type { Subscription } from "rxjs";
-import { debounceTime } from "rxjs/operators";
 import { createPlaysig } from "../utils/create-playsig.js";
 import type { BackContext } from "../types/back-context.js";
-import { merge, filter } from "rxjs";
+import { filter } from "rxjs";
 import type { Hero } from "../types/hero.js";
 import { HeroFactory } from "../utils/hero-factory.js";
 import type { Appellation } from "../types/appellation.js";
@@ -11,78 +10,64 @@ import { getRandomAppellation } from "../utils/get-random-appellation.js";
 import { createPool } from "../utils/create-pool.js";
 import type { Level } from "../types/level.js";
 
-const MATCHMAKING_THROTTLE_TIME = 500;
-
 export function matchmake({
 	dataMapper: { queuers$, createGameWithPoolAndDeleteQueuers },
 	lateMatchmakingTimer,
 }: BackContext): Subscription {
-	const startWhenEight$ = queuers$.pipe(
-		debounceTime(MATCHMAKING_THROTTLE_TIME),
-		filter((queuers) => queuers.length >= 8),
-	);
+	return queuers$
+		.pipe(
+			lateMatchmakingTimer,
+			filter((queuers) => queuers.length > 1),
+		)
+		.subscribe(async (queuers) => {
+			const players = queuers.slice(0, 8);
 
-	const startWhenLate$ = queuers$.pipe(
-		lateMatchmakingTimer,
-		filter((queuers) => queuers.length > 1),
-	);
+			const nicknames = players.reduce(
+				(acc, player) => {
+					acc[player.publicKey] = player.nickname;
+					return acc;
+				},
+				<Record<string, string>>{},
+			);
 
-	const heroFactory = new HeroFactory();
+			const playerHeroes: Record<string, Hero[]> = {};
+			const playerShops: Record<string, Appellation[]> = {};
+			const playerMoney: Record<string, number> = {};
+			const playerLevel: Record<string, Level> = {};
+			const playerBenches: Record<string, Record<number, Hero>> = {};
+			const playerHealths: Record<string, number> = {};
+			const playsig = createPlaysig(players);
+			const pool = createPool(playsig);
 
-	return merge(startWhenEight$, startWhenLate$).subscribe(async (queuers) => {
-		const oldestFirst = [...queuers].sort(
-			(a, b) =>
-				new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
-		);
+			for (const player of players) {
+				playerHeroes[player.publicKey] = [new HeroFactory().build()];
+				playerShops[player.publicKey] = [
+					getRandomAppellation(),
+					getRandomAppellation(),
+					getRandomAppellation(),
+				];
 
-		const players = oldestFirst.slice(0, 8);
+				playerMoney[player.publicKey] = 5;
+				playerLevel[player.publicKey] = 1;
+				playerHealths[player.publicKey] = 10;
+			}
 
-		const nicknames = players.reduce(
-			(acc, player) => {
-				acc[player.publicKey] = player.nickname;
-				return acc;
-			},
-			<Record<string, string>>{},
-		);
-
-		const playerHeroes: Record<string, Hero[]> = {};
-		const playerShops: Record<string, Appellation[]> = {};
-		const playerMoney: Record<string, number> = {};
-		const playerLevel: Record<string, Level> = {};
-		const playerBenches: Record<string, Record<number, Hero>> = {};
-		const playerHealths: Record<string, number> = {};
-		const playsig = createPlaysig(players);
-		const pool = createPool(playsig);
-
-		for (const player of players) {
-			playerHeroes[player.publicKey] = [heroFactory.build()];
-			playerShops[player.publicKey] = [
-				getRandomAppellation(),
-				getRandomAppellation(),
-				getRandomAppellation(),
-			];
-
-			playerMoney[player.publicKey] = 5;
-			playerLevel[player.publicKey] = 1;
-			playerHealths[player.publicKey] = 10;
-		}
-
-		await createGameWithPoolAndDeleteQueuers(
-			{
-				playsig,
-				publicKeys: players.map((player) => player.publicKey),
-				nicknames,
-				playerHeroes,
-				playerShops,
-				playerMoney,
-				playerLevel,
-				playerBenches,
-				playerHealths,
-				phase: Phase.Planning,
-				phaseStartAt: new Date().toISOString(),
-			},
-			pool,
-			players.map((player) => player.publicKey),
-		);
-	});
+			await createGameWithPoolAndDeleteQueuers(
+				{
+					playsig,
+					publicKeys: players.map((player) => player.publicKey),
+					nicknames,
+					playerHeroes,
+					playerShops,
+					playerMoney,
+					playerLevel,
+					playerBenches,
+					playerHealths,
+					phase: Phase.Planning,
+					phaseStartAt: new Date().toISOString(),
+				},
+				pool,
+				players.map((player) => player.publicKey),
+			);
+		});
 }
