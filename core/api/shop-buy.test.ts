eeffb049b73f5f220fb6c9fc2c90c3b5ae87938f
Thomas Riffard <ferdodo@hotmail.fr>
2025-01-10 14:04:50 +0100

    🪙 Fix test case needing more golds

--- core/api/shop-buy.test.ts
+++ core/api/shop-buy.test.ts
@@ -2,10 +2,10 @@ import { test, expect } from "vitest";
 import { asPlayerTransposeBoardToBoard } from "../automations/as-player-transpose-board-to-board.js";
 import { getGame } from "../utils/get-game.js";
 import { asPlayerShopBuy } from "../automations/as-player-shop-buy.js";
-import { withP1HavingTwentyGolds } from "../fixtures/with-p1-having-twenty-golds.js";
+import { withP1HavingTwentyFiveGolds } from "../fixtures/with-p1-having-twenty-golds.js";
 
 test("Buying from shop should fill the first available slot", async () => {
-	const testContext = await withP1HavingTwentyGolds();
+	const testContext = await withP1HavingTwentyFiveGolds();
 	await asPlayerShopBuy(testContext);
 	await asPlayerShopBuy(testContext);
 	await asPlayerTransposeBoardToBoard(testContext);
@@ -17,7 +17,7 @@ test("Buying from shop should fill the first available slot", async () => {
 });
 
 test("Should not exceed the maximum size of the bench", async () => {
-	const testContext = await withP1HavingTwentyGolds();
+	const testContext = await withP1HavingTwentyFiveGolds();
 	await asPlayerShopBuy(testContext);
 	await asPlayerShopBuy(testContext);
 	await asPlayerShopBuy(testContext);
--- core/fixtures/with-p1-having-twenty-golds.ts
+++ core/fixtures/with-p1-having-twenty-golds.ts
@@ -4,13 +4,13 @@ import { getGame } from "../utils/get-game.js";
 import { withTwoPlayerGameStarted } from "./with-two-player-game-started.js";
 import { Phase } from "../types/phase.js";
 
-export async function withP1HavingTwentyGolds(): Promise<TestContext> {
+export async function withP1HavingTwentyFiveGolds(): Promise<TestContext> {
 	const testContext = await withTwoPlayerGameStarted();
 	let money = 0;
 	let game = await getGame(testContext);
 	const publicKey = testContext.frontContexts[0].publicKey || "Error";
 
-	while (money < 20 || game.phase !== Phase.Planning) {
+	while (money < 25 || game.phase !== Phase.Planning) {
 		await goToNextPhase(testContext);
 		game = await getGame(testContext);
 		money = game.playerMoney[publicKey] || 0;
